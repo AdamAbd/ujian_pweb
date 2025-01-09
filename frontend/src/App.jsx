7e128489697956e0871e664e376f2bf3e511277e
Adam Abdurrahman <muhammad.adam.abd@gmail.com>
2025-01-09 16:16:35 +0700

    refactor navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Navbar from "./component/Navbar";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -24,28 +25,7 @@ function App() {
 
   return (
     <div>
-      <nav className="bg-white border-gray-200 shadow-md">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              Flowbite
-            </span>
-          </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-            >
-              Keluar
-            </button>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
       <div className="container mx-auto mt-6">
         <div className="relative overflow-x-auto">
---
+++ frontend/src/component/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <nav className="bg-white border-gray-200 shadow-md">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap">
+                        Notes+
+                    </span>
+                </a>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button
+                        type="button"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                    >
+                        Keluar
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
