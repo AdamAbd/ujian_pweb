643f530f2c3e22045e9de02c52245aae5c1cf8f3
Adam Abdurrahman <muhammad.adam.abd@gmail.com>
2025-01-09 13:54:27 +0700

    update sql

--- backend/db.sql
+++ backend/db.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS notes (
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,               -- Kolom ID pengguna, auto increment
+    username VARCHAR(255) NOT NULL,                  -- Username pengguna, tidak perlu unik, tetapi tidak boleh kosong
+    email VARCHAR(255) NOT NULL UNIQUE,              -- Email pengguna, harus unik dan tidak boleh kosong
+    password VARCHAR(255) NOT NULL,                  -- Password pengguna, akan disimpan dalam format hash
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Waktu pembuatan data pengguna
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Waktu terakhir data diperbarui
+);
